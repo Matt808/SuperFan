47c6945a184f53d7bb5d51b2ca4e945ae834f0cd
Matt808 <54596379+Matt808@users.noreply.github.com>
2019-11-14 15:20:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var a = document.getElementById("game1") 
+var b = document.getElementById("game2") 
+var c = document.getElementById("game3") 
+var d = document.getElementById("game4")
+var e = document.getElementById("game5") 
+var f = document.getElementById("game6") 
+var g = document.getElementById("game7")
